ce1b73f02764cba008ab645e8ebceeaea047c087
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-08-24 19:18:56 +0200

    update lockfiles

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lockfile-archive
 Includes renv.lock files to document the pik-piam R package environment at different points in time
+
+# How to run
+```
+Rscript update_lockfiles.R
+```
---
+++ update_lockfiles.R
@@ -0,0 +1,22 @@
+packagesUrl <- "https://pik-piam.r-universe.dev/src/contrib/PACKAGES"
+pikPiamPackages <- sub("^Package: ", "", grep("^Package: ", readLines(packagesUrl), value = TRUE))
+
+# all pik piam packages and their (optional) dependencies, no optional dependencies of dependencies though
+packages <- unique(c(pikPiamPackages,
+                     unlist(tools::package_dependencies(pikPiamPackages, which = "all", recursive = "strong"))))
+packages <- setdiff(packages, "sr15data") # TODO remove when sr15data is gone or installable
+
+# record global package environment, usually pik-piam packages are up-to-date and
+# CRAN packages are updated only when required by pik-piam packages
+renv::snapshot(lockfile = "conservative.renv.lock", packages = packages, prompt = FALSE)
+
+# install packages into an renv, update all and create lockfile
+callr::r(function(packages) {
+  renv::load() # callr overwrites the .libPaths the renv .Rprofile has set, so load again
+  renv::hydrate(packages = packages)
+  # TODO remove "exclude" when these packages can be built
+  renv::update(exclude = c("units", "stars"))
+  renv::snapshot(lockfile = "../eager.renv.lock", packages = packages)
+  # remove obsolete packages/dependencies
+  renv::restore(lockfile = "../eager.renv.lock", clean = TRUE)
+}, list(packages), wd = "eager_renv", spinner = FALSE, show = TRUE)
